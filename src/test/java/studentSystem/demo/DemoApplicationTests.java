3b9f2a615529c80f26e8ac8997a0f201d76e3011
lizariks <elizachigir@gmail.com>
2025-04-28 17:44:00 +0300

    Initial Spring Boot project setup

---
+++ src/test/java/studentSystem/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package studentSystem.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
